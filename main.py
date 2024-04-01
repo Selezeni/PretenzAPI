8c506f1f418524b5ea32226222381dadd9cf933e
a.seleznev <a.seleznev@avgr.su>
2024-04-01 17:32:28 +0300

    т

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 #from db.models import create_tables
 #from contextlib import asynccontextmanager
 from routers import router as pretenz_router
@@ -15,7 +16,9 @@ from routers import router as pretenz_router
 app = FastAPI(title="AvestaPretenzAPI")
 app.include_router(pretenz_router)
 
-
+@app.get("/")
+async def page_index():
+    return FileResponse("app/templates/index.html")
 
 if __name__ == '__main__':    
     uvicorn.run(app, host='127.0.0.1', port=8000)
